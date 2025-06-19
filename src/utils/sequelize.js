0fbf5eae06fa5716e73f0f957a1b2c246e7d46e6
EhsonjonKomiljonov <ekomiljonov29@gmail.com>
2025-06-19 18:59:51 +0500

    add requirements for railway

--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -9,12 +9,12 @@ if (NODE_ENV !== "production") {
 export const sequelize = new Sequelize(process.env.DB_PUBLIC_HOST, {
   dialect: "postgres",
   logging: false,
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false, // required for Railway
-  //   },
-  // },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   // username: "postgres",
   // protocol: "postgres" ,
   // database: process.env.DB,
