900dce91a30477bf4a4f3db39128340e5118e2b2
EhsonjonKomiljonov <ekomiljonov29@gmail.com>
2025-06-21 18:23:07 +0500

    launch bot only if token is available

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import { app } from "./app.js";
-import { PORT } from "./env.js";
-import './bot/bot.js'
+import { BOT_TOKEN, PORT } from "./env.js";
+import "./bot/bot.js";
 import { bot } from "./bot/bot.js";
 
 app.listen(PORT || 8080, () => {
-  console.log(`-------------- ✅ Server is running on port ${PORT} -------------`);
+  console.log(
+    `-------------- ✅ Server is running on port ${PORT} -------------`
+  );
 
-  bot.launch();
-  console.log("-------------- ✅ Bot started working! -----------------------");
+  if (BOT_TOKEN) {
+    bot.launch();
+    console.log(
+      "-------------- ✅ Bot started working! -----------------------"
+    );
+  }
 });
